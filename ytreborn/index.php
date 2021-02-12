8630e15455b6fad1d662e3eeb1c0c854c3c17296
SarahH12099 <sarahh12099@gmail.com>
2021-02-11 21:36:11 -0500

    Upload Files

---
+++ ytreborn/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="google" content="notranslate"/>
+    <meta name="description" content="">
+    <meta name="author" content="Hollr2099">
+    <link rel="icon" href="favicon.ico">
+    <link rel="apple-touch-icon" href="favicon.ico">
+
+    <title>YouTube Reborn</title>
+
+    <!-- CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <!-- JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
+    <script src="../js/bootstrap-control.js"></script>
+</head>
+<body>
+    <div id="background"></div>
+    <div class="container">
+    <!-- Container -->
+    </div>
+	<script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
